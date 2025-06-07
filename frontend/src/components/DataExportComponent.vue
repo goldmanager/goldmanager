1b03f98b48d1347f759e896137caa716484c8341
fg12111 <github@02n.net>
2025-06-07 16:44:36 +0200

    Fix data export flow

--- frontend/src/components/DataExportComponent.vue
+++ frontend/src/components/DataExportComponent.vue
@@ -4,10 +4,11 @@
       <div><h1>Data Export</h1></div>
       <div v-if="statusMessage">{{ statusMessage }}</div>
       <div v-if="errorMessage" class="error">{{ errorMessage }}</div>
+      <div v-if="showExportStatus">{{ exportStatusMessage }}</div>
       <div>
         <input v-model="exportPassword" type="password" placeholder="Export Password">
         <input v-model="exportPasswordConfirm" type="password" placeholder="Export Password (Confirm)">
-        <button class="actionbutton" @click="exportData">Export</button>
+        <button :class="getExportButtonClass" @click="exportData" :disabled="disableExport">Export</button>
       </div>
     </div>
   </div>
@@ -23,8 +24,18 @@ export default {
       exportPassword: '',
       exportPasswordConfirm: '',
       errorMessage: '',
-      statusMessage: ''
+      statusMessage: '',
+      exportStatusMessage: '',
+      showExportStatus: false,
+      statusIntervalId: null,
+      exportStatus: '',
+      exportStarted: false
     };
+  },
+  computed: {
+    disableExport() {
+      return this.exportStatus === 'RUNNING';
+    }
   },
   methods: {
     async exportData() {
@@ -35,21 +46,76 @@ export default {
         return;
       }
       try {
-        const response = await axios.post('/dataexport/export', {
+        await axios.post('/dataexport/export', {
           password: this.exportPassword
-        }, { responseType: 'blob' });
-        const blob = new Blob([response.data], { type: 'application/zip' });
-        const url = window.URL.createObjectURL(blob);
-        const link = document.createElement('a');
-        link.href = url;
-        link.setAttribute('download', 'goldmanager-export.zip');
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-        this.statusMessage = 'Data exported successfully.';
+        });
+        this.exportStarted = true;
+        this.exportStatusMessage = 'Export started...';
+        this.showExportStatus = true;
+        this.startStatusInterval();
       } catch (error) {
         this.setErrorMessage(error, 'Error exporting data. Please try again later.');
       }
+    },
+    async checkExportStatus() {
+      try {
+        const response = await axios.get('/dataexport/status');
+        this.exportStatus = response.data.status;
+        const backendMsg = response.data.message ? response.data.message : '';
+        if (this.exportStatus === 'RUNNING') {
+          this.exportStatusMessage = backendMsg || 'Export is running...';
+          this.showExportStatus = true;
+          this.startStatusInterval();
+        } else {
+          this.clearStatusInterval();
+          this.showExportStatus = false;
+          if (this.exportStatus === 'PASSWORD_ERROR') {
+            this.errorMessage = 'The data export failed because of an invalid export password.';
+            this.exportStarted = false;
+          } else if (this.exportStatus === 'FAILED') {
+            this.errorMessage = `${backendMsg} Please start the export again.`.trim();
+            this.exportStarted = false;
+          } else if (this.exportStatus === 'SUCCESS' && this.exportStarted) {
+            await this.downloadExport();
+            this.exportStarted = false;
+          }
+        }
+      } catch (error) {
+        this.clearStatusInterval();
+        this.showExportStatus = false;
+      }
+    },
+    async downloadExport() {
+      try {
+        const response = await axios.get('/dataexport/download', { responseType: 'blob' });
+        if (response.data) {
+          const blob = new Blob([response.data], { type: 'application/zip' });
+          const url = window.URL.createObjectURL(blob);
+          const link = document.createElement('a');
+          link.href = url;
+          link.setAttribute('download', 'goldmanager-export.zip');
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+          this.statusMessage = 'Data exported successfully.';
+        }
+      } catch (error) {
+        this.setErrorMessage(error, 'Error downloading export data.');
+      }
+    },
+    startStatusInterval() {
+      if (this.statusIntervalId == null) {
+        this.statusIntervalId = setInterval(this.checkExportStatus, 5000);
+      }
+    },
+    clearStatusInterval() {
+      if (this.statusIntervalId != null) {
+        clearInterval(this.statusIntervalId);
+        this.statusIntervalId = null;
+      }
+    },
+    getExportButtonClass() {
+      return this.disableExport ? 'actionbutton_disabled' : 'actionbutton';
     },
     setErrorMessage(error, defaultMessage) {
       if (error.response != null && error.response.data != null && error.response.data.message != null) {
@@ -58,6 +124,12 @@ export default {
         this.errorMessage = defaultMessage;
       }
     }
+  },
+  async mounted() {
+    await this.checkExportStatus();
+  },
+  beforeUnmount() {
+    this.clearStatusInterval();
   }
 };
 </script>
